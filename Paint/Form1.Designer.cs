f6eefc88e5a6f6a83ee99ea6cba268e2aedb9f88
Glyde <anton123454321045@yandex.by>
2021-02-16 00:01:25 +0300

    First lab

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox1
+            // 
+            this.picBox1.Location = new System.Drawing.Point(12, 12);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(776, 426);
+            this.picBox1.TabIndex = 0;
+            this.picBox1.TabStop = false;
+            this.picBox1.Click += new System.EventHandler(this.picBox_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblInfo.Location = new System.Drawing.Point(296, 417);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(205, 24);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Press left mouse button";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.picBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace Paint
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void picBox_Click(object sender, EventArgs e)
+        {
+            Graphics pic = picBox1.CreateGraphics();
+
+            Pen recPen = new Pen(Color.Red, 2);
+            Rectangle rec = new Rectangle(10, 10, 80, 50);
+            pic.DrawRectangle(recPen, rec);
+
+            Pen EllipsePen = new Pen(Color.Blue, 8);
+            Rectangle EllipseRect = new Rectangle(150, 10, 80, 60);
+            pic.DrawEllipse(EllipsePen, EllipseRect);
+
+            Pen CirclePen = new Pen(Color.Blue, 8);
+            CirclePen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            Rectangle CircleRect = new Rectangle(150, 100, 80, 60);
+            pic.DrawEllipse(CirclePen, CircleRect);
+
+
+            Point LinePT1 = new Point(300, 10);
+            Point LinePT2 = new Point(350, 150);
+            Pen LinePen = new Pen(Color.Green, 2);
+            pic.DrawLine(LinePen, LinePT1, LinePT2);
+
+            SolidBrush brushDots = new SolidBrush(Color.Magenta);
+            Point[] dots = {new Point(100,150), new Point(200, 250),
+                            new Point(100,350), new Point(150, 250) };
+            pic.FillPolygon(brushDots, dots);
+
+            Pen curve = new Pen(Color.Coral, 4);
+            Pen lines = new Pen(Color.DeepPink, 4);
+            Point[] points = {new Point(500, 10), new Point(550, 110),
+                              new Point(680, 100), new Point(700, 400),
+                              new Point(500, 350)};
+            pic.DrawCurve(curve, points);
+            pic.DrawLines(lines, points);
+
+
+        }
+    }
+}
